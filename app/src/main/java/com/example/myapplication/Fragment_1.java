8897c8cb274b054e303e07f4ea4f45e9acca8b6f
yangtao <13625606990@163.com>
2021-04-22 11:04:40 +0800

    + Google Admob 横幅 插屏 开屏

--- app/src/main/java/com/example/myapplication/Fragment_1.java
+++ app/src/main/java/com/example/myapplication/Fragment_1.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -12,13 +13,55 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+// Banner
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+// Insert
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+
+// App Open
+import android.app.Application;
+
 public class Fragment_1 extends AppCompatActivity {
-    private int img[] = {R.id.image_1,R.id.image_2,R.id.image_3,R.id.image_4};
+    private int img[] = {R.id.image_1, R.id.image_2, R.id.image_3, R.id.image_4};
+    private AdView mAdView;
+    private InterstitialAd mInterstitialAd;
+    private static AppOpenManager appOpenManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_1);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        appOpenManager = new AppOpenManager(this);
+
+        // Banner
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        // Insert
+        InterstitialAd.load(this, "ca-app-pub-3940256099942544/1033173712", adRequest, new InterstitialAdLoadCallback() {
+            @Override
+            public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                // The mInterstitialAd reference will be null until
+                // an ad is loaded.
+                mInterstitialAd = interstitialAd;
+//                Log.i(TAG, "onAdLoaded");
+            }
+        });
 
         // 获取屏幕宽高
         DisplayMetrics dm = new DisplayMetrics();
@@ -29,24 +72,23 @@ public class Fragment_1 extends AppCompatActivity {
         // 动态添加 Fragment
         FragmentManager fm = getSupportFragmentManager();
 
-        Fragment frag =  fm.findFragmentById(R.id.imageList);
+        Fragment frag = fm.findFragmentById(R.id.imageList);
 
-        if(screenWidth > screenHeight){ // 横屏
-            if(frag != null){
+        if (screenWidth > screenHeight) { // 横屏
+            if (frag != null) {
                 fm.beginTransaction().remove(frag).commitNow();
             }
-        }else{ //竖屏
-            if(frag == null){
+        } else { //竖屏
+            if (frag == null) {
                 fm.beginTransaction().add(R.id.imageList, new SecondFragment()).commitNow();
             }
         }
 
 
-
         Button btn = findViewById(R.id.changeButton2);
 //        btn.setOnClickListener(new myOnClick());
 
-        btn.setOnClickListener(new View.OnClickListener(){
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 TextView t = findViewById(R.id.firstText);
@@ -61,16 +103,16 @@ public class Fragment_1 extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         final ImageView image = (ImageView) findViewById(R.id.view_image);
-        for(int i = 0; i < img.length; i++) {
+        for (int i = 0; i < img.length; i++) {
             ImageView iv = (ImageView) findViewById(img[i]);
-            if(iv != null){
+            if (iv != null) {
                 iv.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
 
                         Log.d("被点击了", v.toString());
                         ImageView view = (ImageView) v;
-                        if(image.getDrawable() == view.getDrawable()){
+                        if (image.getDrawable() == view.getDrawable()) {
                             image.setImageDrawable(getResources().getDrawable(R.drawable.ic_launcher_foreground));
                             return;
                         }
@@ -91,9 +133,15 @@ public class Fragment_1 extends AppCompatActivity {
         }
     }
 
-    public void changeTheText(View view){
+    public void changeTheText(View view) {
         TextView t = findViewById(R.id.firstText);
         t.setText(R.string.hello_first_fragment);
         this.setTitle(R.string.hello_first_fragment);
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show(Fragment_1.this);
+        } else {
+            Log.d("TAG", "The interstitial wasn't loaded yet.");
+        }
     }
 }
