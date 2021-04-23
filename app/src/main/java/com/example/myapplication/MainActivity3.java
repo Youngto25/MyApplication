39634d6ef157a8f5671d932d9412365abb80067f
yangtao <13625606990@163.com>
2021-04-23 17:55:17 +0800

    + Facebook Audience Network 插屏广告

--- app/src/main/java/com/example/myapplication/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -3,12 +3,101 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.ads.Ad;
+import com.facebook.ads.AdError;
+import com.facebook.ads.AudienceNetworkAds;
+import com.facebook.ads.InterstitialAd;
+import com.facebook.ads.InterstitialAdListener;
 
 public class MainActivity3 extends AppCompatActivity {
+    private InterstitialAd interstitialAd;
+    private String TAG = "Activity4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        AudienceNetworkAds.initialize(this);
+
+        interstitialAd = new InterstitialAd(this, "IMG_16_9_APP_INSTALL#4523648584335513_4523653341001704");
+
+        InterstitialAdListener interstitialAdListener = new InterstitialAdListener() {
+            @Override
+            public void onInterstitialDisplayed(Ad ad) {
+                // Interstitial ad displayed callback
+                Log.e(TAG, "Interstitial ad displayed.");
+            }
+
+            @Override
+            public void onInterstitialDismissed(Ad ad) {
+                // Interstitial dismissed callback
+                Log.e(TAG, "Interstitial ad dismissed.");
+            }
+
+            @Override
+            public void onError(Ad ad, AdError adError) {
+                // Ad error callback
+                Log.e(TAG, "Interstitial ad failed to load: " + adError.getErrorMessage());
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+                // Interstitial ad is loaded and ready to be displayed
+                Log.d(TAG, "Interstitial ad is loaded and ready to be displayed!");
+                // Show the ad
+//                interstitialAd.show();
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+                // Ad clicked callback
+                Log.d(TAG, "Interstitial ad clicked!");
+            }
+
+            @Override
+            public void onLoggingImpression(Ad ad) {
+                // Ad impression logged callback
+                Log.d(TAG, "Interstitial ad impression logged!");
+            }
+        };
+
+        // For auto play video ads, it's recommended to load the ad
+        // at least 30 seconds before it is shown
+        interstitialAd.loadAd(
+                interstitialAd.buildLoadAdConfig()
+                        .withAdListener(interstitialAdListener)
+                        .build());
+
+        Button bt = (Button)findViewById(R.id.show_ad_button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity3.this.showAdWithDelay();
+            }
+        });
+    }
+
+    public void showAdWithDelay() {
+        Log.d(TAG, "showAdWithDelay");
+        /**
+         * Here is an example for displaying the ad with delay;
+         * Please do not copy the Handler into your project
+         */
+        if(interstitialAd == null || !interstitialAd.isAdLoaded()) {
+            return;
+        }
+        Log.d(TAG, "showAdWithDelay111111");
+        // Check if ad is already expired or invalidated, and do not show ad if that is the case. You will not get paid to show an invalidated ad.
+        if(interstitialAd.isAdInvalidated()) {
+            return;
+        }
+        Log.d(TAG, "showAdWithDelay222222s");
+        // Show the ad
+        interstitialAd.show();
     }
 }
