3f3f46e1ab959bc4d6d6a3bec03bf40ef823b8ae
yangtao <13625606990@163.com>
2021-04-25 11:05:23 +0800

    ! Firebase 消息推送与埋点

--- app/src/main/java/com/example/myapplication/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,8 +15,6 @@ import com.facebook.ads.AdError;
 import com.facebook.ads.AudienceNetworkAds;
 import com.facebook.ads.InterstitialAd;
 import com.facebook.ads.InterstitialAdListener;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailabilityLight;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
@@ -30,6 +30,69 @@ public class MainActivity3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
+        AudienceNetworkAds.initialize(this);
+
+        initFacebookAd();
+
+        intGoogleMessage();
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        Button sb = (Button)findViewById(R.id.send_log_button);
+        sb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity3.this.sendLogEvent();
+            }
+        });
+    }
+
+    // analytics 发送事件
+    private void sendLogEvent() {
+        Log.d(TAG, "click send button");
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "init");
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "init");
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "hello world1");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+        Log.d(TAG, "send end");
+    }
+
+    // google cloud message
+    private  void intGoogleMessage() {
+        //是否支持google
+//        int code = GoogleApiAvailabilityLight.getInstance().isGooglePlayServicesAvailable(this);
+//
+//        Log.w("FcmReceiverService", String.valueOf(code));
+//
+//        Log.w("FcmReceiverService", String.valueOf(ConnectionResult.SUCCESS));
+//
+//        //
+//        Log.w("FcmReceiverService", String.valueOf(GoogleApiAvailabilityLight.getInstance().isUserResolvableError(code)));
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+//                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(TAG, token);
+                    }
+                });
+    }
+
+
+
+    // 初始化facebook 广告
+    private void initFacebookAd() {
         interstitialAd = new InterstitialAd(this, "IMG_16_9_APP_INSTALL#4523648584335513_4523653341001704");
 
         InterstitialAdListener interstitialAdListener = new InterstitialAdListener() {
@@ -86,44 +149,6 @@ public class MainActivity3 extends AppCompatActivity {
                 MainActivity3.this.showAdWithDelay();
             }
         });
-
-        //是否支持google
-//        int code = GoogleApiAvailabilityLight.getInstance().isGooglePlayServicesAvailable(this);
-//
-//        Log.w("FcmReceiverService", String.valueOf(code));
-//
-//        Log.w("FcmReceiverService", String.valueOf(ConnectionResult.SUCCESS));
-//
-//        //
-//        Log.w("FcmReceiverService", String.valueOf(GoogleApiAvailabilityLight.getInstance().isUserResolvableError(code)));
-
-        FirebaseMessaging.getInstance().getToken()
-                .addOnCompleteListener(new OnCompleteListener<String>() {
-                    @Override
-                    public void onComplete(@NonNull Task<String> task) {
-                        if (!task.isSuccessful()) {
-                            Log.w("FcmReceiverService", "Fetching FCM registration token failed", task.getException());
-                            return;
-                        }
-
-                        // Get new FCM registration token
-                        String token = task.getResult();
-
-                        // Log and toast
-//                        String msg = getString(R.string.msg_token_fmt, token);
-                        Log.d("FcmReceiverService", token);
-                    }
-                });
-
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-
-        AudienceNetworkAds.initialize(this);
-
-        Bundle bundle = new Bundle();
-        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "init");
-        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "init");
-        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "hello world");
-        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
     }
 
     public void showAdWithDelay() {
@@ -133,6 +158,7 @@ public class MainActivity3 extends AppCompatActivity {
          * Please do not copy the Handler into your project
          */
         if(interstitialAd == null || !interstitialAd.isAdLoaded()) {
+            Log.d(TAG, "showAdWithDelay is null or isAdLoaded");
             return;
         }
         Log.d(TAG, "showAdWithDelay111111");
